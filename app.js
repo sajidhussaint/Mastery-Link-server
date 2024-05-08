8d3b85d352856adca5271eb296881229c253822e
sajid <sajidtmpm@gmail.com>
2024-05-08 20:28:20 +0530

    feat:Added cors Vercel

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://mastery-link-client.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH"],
     credentials: true,
   })
